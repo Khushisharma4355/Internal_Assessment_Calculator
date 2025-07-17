a85efeb1531468249fa319627dce93b88b38f74a
Khushi Sharma <ks9459879@gmail.com>
2025-07-17 13:07:46 +0530

    schema added

---
+++ server/model/Student.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db";
+const Student=sequelize.define("Student",{     //create a table named Students in the db
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    classs:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    course:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    rollno:{
+        type:DataTypes.BIGINT,
+        allowNull:false
+    },
+    registrationNo:{
+        type:DataTypes.BIGINT,
+        allowNull:false
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
+    parent:{
+        type:DataTypes.STRING,
+    },
+
+});
+export default Student;
