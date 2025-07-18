f3f55f797b5e0196f0923397e6b357a7fcf5a9ce
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-18 17:43:22 +0530

    .env added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css'
 import {Route,Routes} from "react-router-dom"
-import {Navv} from "./Components/NavBar/Navbar"
 import { StuHome } from './Screens/Students/StuHome'
 function App() {
 
   return (
     <>
-    <Navv/>
       <Routes>
         <Route path="/" element={<StuHome/>}/>
       </Routes>
--- client/src/Screens/Students/StuHome.jsx
+++ client/src/Screens/Students/StuHome.jsx
@@ -1,7 +1,23 @@
+import {Navv} from "../../Components/Students/NavBar/Navbar"
+import { Row,Col,Container } from "react-bootstrap"
 export const StuHome=()=>{
     return(
         <>
+        <Container fluid>
+            <Row >
+          <Col md={2} className="bg-warning vh-100 p-3">
+                <Navv/></Col>
+          <Col className="d-flex  justify-content-center">
         <h3>Welcome Students!!!</h3>
+                </Col>
+            </Row>
+        </Container>
         </>
     )
 }
+
+// vh-100 on the sidebar keeps it full-height.
+
+// p-3 adds padding to the sidebar.
+
+// d-flex align-items-center justify-content-center centers the message in the main content area.
