8a1c45de089c6739031bbc7f82b0def7961e4dbd
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-19 22:59:15 +0530

    pages added

---
+++ client/src/Screens/Students/Assesments.jsx
@@ -0,0 +1,18 @@
+// import {Navv} from "../../Components/Students/NavBar/Navbar"
+import { Row,Col,Container } from "react-bootstrap"
+import {Stunav} from "../../Components/Students/Stunav"
+export const Assesments=()=>{
+    return(
+        <>
+<Stunav/>
+         <Container className="bg-light">
+            <Row >
+          <Col className="d-flex  justify-content-center">
+        <h3>Hiii Assesments !!!</h3>
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
+
