04cf926229d273555a6658d7e32693d4466f9c28
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-23 00:51:42 +0530

    routes updated

---
+++ client/src/Screens/Admin/AdminHome.jsx
@@ -0,0 +1,18 @@
+// import {Navv} from "../../Components/Students/NavBar/Navbar"
+import { Row,Col,Container } from "react-bootstrap"
+import { AdminNav } from "../../Components/Admin/AdminNav"
+export const AdminHome=()=>{
+    return(
+        <>
+<AdminNav/>
+        <Container className="bg-light">
+            <Row >
+          <Col className="d-flex  justify-content-center">
+        <h3>Welcome Admin!!!</h3>
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
+
