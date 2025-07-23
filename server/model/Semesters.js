1d4f1ae93e3e5795cb83edaad2eb8d4b66d6d9dc
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-23 12:26:54 +0530

    semesters added

--- server/model/Semesters.js
+++ server/model/Semesters.js
@@ -1,2 +1,16 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/db";
+import sequelize from "../config/db.js";
+const Semesters=sequelize.define("Semesters",{
+    sem_id:{
+        type:DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    course_id:{
+        type:DataTypes.STRING,
+        allowNull:true,
+        
+    },
+
+})
+export default Semesters
