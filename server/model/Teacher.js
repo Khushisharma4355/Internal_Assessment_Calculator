3b401c6c0fe0c4102a2e706e84b058d3b0f833a8
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-26 16:48:52 +0530

    assesments added

--- server/model/Teacher.js
+++ server/model/Teacher.js
@@ -1,43 +1,46 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
-import Department from "./Department.js";
-const Teacher = sequelize.define("Teacher", {
-    emp_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    emp_email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    emp_phone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [10, 15],
-            is: /^[0-9]+$/i
-        }
-    },
-    emp_id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        unique: true,
-        allowNull: false,
-    },
-    dep_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-            model: "Departments",
-            key: "dep_id",
-            onDelete: "CASCADE",
-            onUpdate: "CASCADE"
-        }
-    },
-}, {
-    tableName: "Teachers",
-    timestamps: true
-}
-)
+import Department from "./Department.js"; // This import is used for associations below
 
-export default Teacher
+const Teacher = sequelize.define("Teacher", {
+  emp_id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false,
+    unique: true
+  },
+  emp_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  emp_email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  emp_phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [10, 15],
+      is: /^[0-9]+$/i
+    }
+  },
+  dep_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: Department,  
+      key: "dep_id"
+    },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  }
+}, {
+  tableName: "Teachers",
+  timestamps: true
+});
+
+export default Teacher;
