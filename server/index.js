3f3bb0765d5aa17972b35b5083ed09c81e462949
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-30 15:23:09 +0530

    teacher resolvers and typedef's added

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
   "/graphql",
   expressMiddleware(server)
 );
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 syncDatabase(); 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/graphql`);
