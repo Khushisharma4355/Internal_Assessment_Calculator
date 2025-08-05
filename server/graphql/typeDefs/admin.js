a218c8e3798fcd4c0e6dabd8c41c2e4623b49ca4
Khushi Sharma <ks9459879@gmail.com>
2025-08-05 14:10:46 +0530

    add user modal added

---
+++ server/graphql/typeDefs/admin.js
@@ -0,0 +1,16 @@
+import { gql } from "graphql-tag";
+
+const adminTypeDef = gql`
+  type Admin {
+    emp_id: String!
+    teacher: Teacher
+    teacherCount:Int
+    studentCount:Int
+    courseCount:Int
+  }
+  extend type Query {
+    getAdmin(emp_id: String!): Admin
+  }
+`;
+
+export default adminTypeDef;
